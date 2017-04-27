0912ed660f35f7714b7dae64d5a31303b2ce13a0
Joey Schutz <schutzj@kenyon.edu>
2017-04-27 18:30:42 -0400

    updated to work with Index class

--- consoleServer.cpp
+++ consoleServer.cpp
@@ -10,42 +10,39 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-#include <index.h>
+#include <WORD_INDEX_H.h>
 
 using namespace std;
 
 
 int main() 
-	{ 
-     
+{ 
 	string rootDirectory = "rootdirectory"; 
-
-	setOfBooks = Index(rootDirectory); //builds the map 
-
-
+	Index index(rootDirectory);
+	index.Build();
 	do 
+	{
+
+		//word = getInput(); 
+		cout << "Enter word: " ; 
+		cin >> word; 
+		cout << endl; 
+
+		vector<string>instancesSet = index.GetInstancesOf(word);
+
+
+		for (int i=0; i < instancesSet.size(); i++)
 		{
+			cout << instancesSet[i] << endl; 
+		}
 
-			//word = getInput(); 
-			cout << "Enter word: " ; 
-			cin >> word; 
-			cout << endl; 
-	
-			vector<string>instancesSet = Index.GetInstancesOf(word);
-	
-	
-			for (int i=0; i < instancesSet.size(); i++)
-			{
-				cout << instancesSet[i] << endl; 
-			}
-			
-			cout << "$END"; 
+		cout << "$END"; 
 
-			cout << "Hit A to go again" << endl;
-			//checks loop to go again
-			cin >> again;
+		cout << "Hit A to go again" << endl;
+		//checks loop to go again
+		cin >> again;
 
-		} while (again == 'A' || again == 'a');
+	} while (again == 'A' || again == 'a');
 
-	}
+}
     
