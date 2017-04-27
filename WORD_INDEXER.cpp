0af20320a55282af2a0ad9b1cb1b2d5e992ddf6c
Joey Schutz <schutzj@kenyon.edu>
2017-04-27 18:09:54 -0400

    uploaded word_indexer class

---
+++ WORD_INDEXER.cpp
@@ -0,0 +1,153 @@
+#include "WORD_INDEXER_H.h"
+
+using namespace std;
+
+Index::Index()
+{
+	dirRoot = "";
+}
+
+Index::Index(string rootDirectory)
+{
+	dirRoot = rootDirectory;
+}
+
+string Index::GetDirRoot()
+{
+	return dirRoot;
+}
+
+void Index::Reset()
+{
+	dirRoot = "";
+	index.clear();
+	bookIDRef.clear();
+}
+
+void Index::ChangeDirRoot(string newRoot)
+{
+	dirRoot = newRoot;
+}
+
+void Index::Build()
+{	
+	ifstream infile;
+	string word, line;
+	
+	if(dirRoot.empty())		//if filename has not been provided, return
+	{
+		cout << "\n\nERROR: No file given.\n\n";
+		return;
+	}
+	
+	bookIDRef.push_back(dirRoot);
+	CarefulOpenIn(infile, dirRoot);
+	int pos= 0;
+	while(!infile.fail())
+	{
+		getline(infile, line);
+		istringstream iss(line);
+		while(iss >> word)
+		{
+			MakeLower(word);			//store word as lower
+			if(!index[word].empty())			//add to current WordLocations
+			{
+				index[word][0].locations.push_back(pos);			
+			}
+			else			//otherwise, no WordLocations stored, so one must be added 
+			{
+				WordLocations loc;
+				loc.locations.push_back(pos);
+				index[word].push_back(loc);
+			}
+			
+		}
+		
+		pos = infile.tellg();
+	}
+	
+	infile.close();
+}
+
+vector<string> Index::GetInstancesOf(string word)
+{	
+	vector<WordLocations> locations;
+
+	MakeLower(word);		//make sure word is normalized
+	locations = GetLocations(word);
+	stringstream iss;
+	iss << locations.size();
+
+	vector<string> lines;
+	for(int i = 0; i < locations.size(); i++)
+	{
+		vector<string> temp = GetInstancesOfWordInFile(dirRoot, locations[i].locations);		//get every instance of word in each file
+		for(int i = 0; i < temp.size(); i++)		
+		{
+			lines.push_back(temp.at(i));			//and store it in lines
+		}
+	}
+	
+	return lines;
+}
+
+vector<string> Index::GetInstancesOfWordInFile(string filePath, vector<int> positions)
+{
+	vector<string> lines;
+	ifstream infile;
+	CarefulOpenIn(infile, filePath);
+	stringstream iss;
+	iss << positions.size();
+	
+	string line, word;
+	int last_pos = -1;
+	for(int i = 0; i < positions.size(); i++)			//output lines starting at each position
+	{
+		infile.seekg(positions[i], infile.beg);
+		getline(infile, line);
+		if(positions[i] != last_pos)							//don't output same line multiple times
+		{
+			istringstream iss(line);
+			line = "";
+			while(iss >> word)					//add each word into line
+			{
+				line += word + " ";
+			}
+		}
+		last_pos = positions[i];
+		lines.push_back(line);
+	}
+	
+	infile.close();
+	return lines;
+}
+
+vector<WordLocations> Index::GetLocations(string word)
+{
+	map<string, vector<WordLocations> >::iterator it;
+	vector<WordLocations> empty_locations;							//returned if word is not found in index
+	
+	it = index.find(word);
+	if(it == index.end())					//word is not found
+		return empty_locations;
+	else
+		return index[word];
+}
+
+void Index::CarefulOpenIn(ifstream& infile, string name)
+{
+      infile.open(name.c_str(), ios::binary);			//was getting wrong positions without opening binary
+      if(infile.fail())
+      {
+         cout << "\n\n\nThere was an error opening the input file " << name << ".\n\n";
+      }
+      return;
+}
+
+void Index::MakeLower(string& word)
+{
+	for(int i = 0; i < word.length(); i++)
+	{
+		word[i] = tolower(word[i]);
+	}
+}
---
+++ WORD_INDEXER_H.h
@@ -0,0 +1,55 @@
+#ifndef WORD_INDEXER_H
+#define WORD_INDEXER_H
+
+#include<string>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<fstream>
+#include<cstdlib>
+#include<ctype.h>
+#include<stdio.h>
+#include<cstdio>
+#include<sstream>
+#include<algorithm>
+
+using namespace std;
+
+struct WordLocations
+{
+	int bookID;
+	vector<int> locations;
+};
+
+class Index
+{
+public:
+	Index();		//creates index with no reference to a directory too
+	
+	Index(string rootDirectory);		//creates index to build from the directory root
+	
+	string GetDirRoot();		//for testing
+	
+	void ChangeDirRoot(string newRoot);	//changes directory root to newRoot, and clears out index and bookIDRef
+	
+	void Reset();			//sets dirRoot to empty string, clears index, clears bookIDRef
+	
+	void Build();			//builds index of every word in every file in the directory root
+	
+	vector<string> GetInstancesOf(string word);		//returns every line in every file in every subdirectory of dirRoot containing the word "word"
+	
+private:
+	void CarefulOpenIn(ifstream& infile, string name);		//opens file, exits program if attempt fails
+	
+	vector<WordLocations> GetLocations(string word);				//returns WordLocations of word in index--returns an empty WordLocations if word not in index
+	
+	vector<string> GetInstancesOfWordInFile(string filePath, vector<int> positions);		//returns all instances of word in filePath
+	
+	void MakeLower(string& word);
+
+	string dirRoot;
+	map<string, vector<WordLocations> > index;
+	vector<string> bookIDRef;
+};
+
+#endif
