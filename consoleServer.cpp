152e8930cd98058b126fec26d5aed9f6d27656ff
natalietwitchell <natalie.twitchell@gmail.com>
2017-05-05 18:10:26 -0400

    cgi working?

--- consoleServer.cpp
+++ consoleServer.cpp
@@ -11,40 +11,60 @@
 #include <string>
 #include <vector>
 #include "WORD_INDEXER_H.h"
+#inlcude "fifo.h"
 
 using namespace std;
+
+/* Fifo names */
+string receive_fifo = "GutenbergRequest";
+string send_fifo = "GutenbergReply";
 
 
 int main() 
 { 
+	string inMessage, outMessage;
+	
 	string rootDirectory = "/home/skon/books/"; 
 	Index index(rootDirectory);
 	index.Build();
-	char again;
+	
+	while(1){//this will keep running 
 	vector<string> matchLines;
-	do 
+	
 	{
 		string word;
-
-		//word = getInput(); 
-		cout << "Enter word: " ; 
-		cin >> word; 
-		cout << endl; 
+		
+		//gets the input from CGI
+		recfifo.openread();
+        	inMessage = recfifo.recv();
+		
+		cout << "Word: " << endl; 
 
 		matchLines = index.GetInstancesOf(word);
+		
+		//initiates write lock 
+		
+		 sendfifo.openwrite();
+        	sendfifo.send("</p>");
 
 		 for (int i=0; i < matchLines.size(); i++)
 		 {
 		 	cout << matchLines[i] << endl; 
+			outMessage = matchLines[i]; 
+			sendfifo.send(outMessage); 
+			sendfifo.send("</p>");
+			 //ships out lines to website. 
 		 }
 
+		//closes up
 		cout << "$END"; 
+		sendfifo.send("$END");
+        	sendfifo.fifoclose();
+       		recfifo.fifoclose();
 
-		cout << "Hit A to go again" << endl;
-		//checks loop to go again
-		cin >> again;
+		
 
-	} while (again == 'A' || again == 'a');
+	}
 
 }
     
