ee791f03ebb9a8cd3f9a3d5567a6e4cb8bae3e6b
Joey Schutz <schutzj@kenyon.edu>
2017-05-08 18:38:40 -0400

    javascript communcates with CGI properly

--- searchbooksAjax.cpp
+++ searchbooksAjax.cpp
@@ -43,9 +43,9 @@ int main()
     // Create AJAX objects to recieve information from web page.
     form_iterator word = cgi.getElement("word");
 
-    // create the FIFOs for communication
-    Fifo recfifo(receive_fifo);
-    Fifo sendfifo(send_fifo);
+    // // create the FIFOs for communication
+    // Fifo recfifo(receive_fifo);
+    // Fifo sendfifo(send_fifo);
 
     cout << "Content-Type: text/plain\n\n";
 
@@ -53,20 +53,25 @@ int main()
     // stword = StringToUpper(stword); //gets to upper case 
 
 
-    sendfifo.openwrite();
-    sendfifo.send(searchWord);			//send the word to the server
+    // sendfifo.openwrite();
+    // sendfifo.send(searchWord);			//send the word to the server
 
-    recfifo.openread();
-    cout << "</p>";
-	string reply;
-    do 					//get messages from server
-	{
-        reply = recfifo.recv();
-        cout << "</p>" << reply;				//and send it back to the website
-    } while (reply.find("$END") == string::npos);
+    // recfifo.openread();
+    // cout << "</p>";
+	// string reply;
+    // do 					//get messages from server
+	// {
+        // reply = recfifo.recv();
+        // cout << "</p>" << reply;				//and send it back to the website
+    // } while (reply.find("$END") == string::npos);
 	
-    recfifo.fifoclose();
-    sendfifo.fifoclose();			//close FIFOs
+    // recfifo.fifoclose();
+    // sendfifo.fifoclose();			//close FIFOs
+	
+	for(int i = 0; i < 1000; i++)
+	{
+		cout << "</p>" << searchWord;
+	}
 	
     return 0;
 }
